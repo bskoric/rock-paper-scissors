743e9a124c5838fdbb97cbd1dbf7352c5dac2689
Branko Skoric <b.skoric@netconomy.net>
2022-12-17 18:27:36 +0100

    master: Put 100 rounds

--- src/main/java/com/branko/game/RockPapersScissorsGame.java
+++ src/main/java/com/branko/game/RockPapersScissorsGame.java
@@ -6,11 +6,13 @@ import com.branko.game.domain.Statistics;
 
 public class RockPapersScissorsGame {
 
+    private static final int NUMBER_OF_ROUNDS = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to the game");
 
         GameController gameController = new GameController();
-        Game gameResult = gameController.playGame(5);
+        Game gameResult = gameController.playGame(NUMBER_OF_ROUNDS);
         Statistics statistics = gameResult.getStatistics();
 
         System.out.printf("%s wins %s of %s games %n",
